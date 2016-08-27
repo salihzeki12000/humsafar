f2dfc9576917dec8ae28c4f1b5e1da3ec788759d
sohan1 <shn619@gmail.com>
2016-08-27 19:09:26 +0530

    country must do

--- js/controllers.js
+++ js/controllers.js
@@ -815,8 +815,10 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
          {
          img:"img/destination/taj.jpg",
          title:"Taj Mahal",
-         description:"<p>A symbol of an eternal love story etched out in the world’s most marvellous structures in the world, the Taj Mahal epitomises one of the greatest romances in the history of mankind. Designated as a UNESCO World Heritage Site and one of the Seven Wonders of the World, this ‘tear-drop on the cheek of time’, as Rabindranath Tagore described it, is regarded as the best example of Mughal architecture and the country’s rich history. Marvel at the great marble monument ornamented with 28 types of precious and semi-precious stones for the inlay work. With its four minarets and the red sandstone mosque, this fascinating monument is a sight to behold.</p>"
+         description:"<p>A symbol of an eternal love story etched out in the world’s most marvellous structures in the world, the Taj Mahal epitomises one of the greatest romances in the history of mankind. Designated as a UNESCO World Heritage Site and one of the Seven Wonders of the World, this ‘tear-drop on the cheek of time’, as Rabindranath Tagore described it, is regarded as the best example of Mughal architecture and the country’s rich history. Marvel at the great marble monument ornamented with 28 types of precious and semi-precious stones for the inlay work. With its four minarets and the red sandstone mosque, this fascinating monument is a sight to behold.</p>",
+         link:"www.tajmahaltripindia.com"
        }
+
      ];
      $scope.placesimage=[
        'img/destination/taj.jpg',
