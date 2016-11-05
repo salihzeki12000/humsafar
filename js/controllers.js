339fb8ab4ffb90e621d4558bc5e18113a1d971f8
ipratikminat <pratik.wohlig@gmail.com>
2016-11-05 15:26:16 +0530

    map chanhes

--- js/controllers.js
+++ js/controllers.js
@@ -8,6 +8,7 @@ var globalGetProfile = function (data, status) {
 };
 var pointsForLine;
 var line = [];
+var travelPath;
 var initMap = function () {};
 var map;
 angular.module('phonecatControllers', ['templateservicemod', 'mylife', 'ongojourney', 'navigationservice', 'ui.bootstrap', 'ui.select', 'ngAnimate', 'ngSanitize', 'angular-flexslider', 'angularFileUpload', 'ngImgCrop', 'mappy', 'wu.masonry', 'ngScrollbar', 'ksSwiper', 'ui.tinymce'])
@@ -803,76 +804,6 @@ angular.module('phonecatControllers', ['templateservicemod', 'mylife', 'ongojour
       console.log(err);
     });
 
-    //amits map
-    {
-      // initMap = function () {
-      //   var tardeo = {
-      //     lat: 18.9692098,
-      //     lng: 72.81516999
-      //   };
-      //   // Create a new StyledMapType object, passing it an array of styles,
-      //   // and the name to be displayed on the map type control.
-      //   var map = new google.maps.Map(document.getElementById('map'), {
-      //     zoom: 12,
-      //     center: tardeo,
-      //     disableDefaultUI: true
-      //   });
-      //   var styledMapType = new google.maps.StyledMapType(
-      //     [{
-      //       stylers: [{
-      //           hue: '#b3d2fe'
-      //         },
-      //         // { hue: '#000' },
-      //       ]
-      //     }], {
-      //       name: 'Styled Map'
-      //     });
-
-      //   var contentString = '<div id="content">' +
-      //     '<div id="siteNotice">' +
-      //     '</div>' +
-      //     '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-      //     '<div id="bodyContent">' +
-      //     '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      //     'sandstone rock formation in the southern part of the ' +
-      //     'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
-      //     'south west of the nearest large town, Alice Springs; 450&#160;km ' +
-      //     '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
-      //     'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
-      //     'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
-      //     'Aboriginal people of the area. It has many springs, waterholes, ' +
-      //     'rock caves and ancient paintings. Uluru is listed as a World ' +
-      //     'Heritage Site.</p>' +
-      //     '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-      //     'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
-      //     '(last visited June 22, 2009).</p>' +
-      //     '</div>' +
-      //     '</div>';
-
-      //   var infowindow = new google.maps.InfoWindow({
-      //     content: contentString,
-      //     maxWidth: 200
-      //   });
-
-      //   var marker = new google.maps.Marker({
-      //     position: tardeo,
-      //     map: map,
-      //     title: 'Tardeo (Ayers Rock)'
-      //   });
-      //   marker.addListener('click', function () {
-      //     infowindow.open(map, marker);
-      //   });
-      //   map.mapTypes.set('styled_map', styledMapType);
-      //   map.setMapTypeId('styled_map');
-      // }
-
-      // $scope.$on('$viewContentLoaded', function(){
-      //   $timeout(function() {
-      //     initMap();
-      //   },100);
-      //  });
-    }
-
     //mapStyle
     var mapStyle = [{
       "featureType": "landscape.man_made",
@@ -1022,31 +953,25 @@ angular.module('phonecatControllers', ['templateservicemod', 'mylife', 'ongojour
     line = _.map(centers, function () {
       return {};
     });
-    console.log(line);
-
-    // {
-    //   lat: 19.238368,
-    //   lng: 73.129807
-    // }, {
-    //   lat: 19.287449,
-    //   lng: 73.545227
-    // }, {
-    //   lat: 18.916314,
-    //   lng: 73.318634
-    // }
 
     initMap = function () {
 
-      var firstTime = true;
 
       if (google) {
-        var map;
         var bounds = new google.maps.LatLngBounds();
         var step = 0;
         var numSteps = 100; //Change this to set animation resolution
 
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          draggable: true,
+          animation: google.maps.Animation.DROP,
+          center: center,
+          // styles: mapStyle
+        });
+
         function redLineDraw(i, departure, arrival, percentComplete, value) {
-          setMarker(true,centers[0]);
+          setMarker(true, centers[0]);
           var xdiff = (centers[i].lat - centers[i - 1].lat);
           var ydiff = (centers[i].lng - centers[i - 1].lng);
           if (Math.abs(xdiff) < 4) {
@@ -1064,8 +989,7 @@ angular.module('phonecatControllers', ['templateservicemod', 'mylife', 'ongojour
             strokeOpacity: 1,
             scale: 4
           };
-
-          if(percentComplete>=100){
+          if (percentComplete >= 100) {
             setMarker(true, centers[i]);
           }
           if (_.isEmpty(line[i])) {
@@ -1082,8 +1006,8 @@ angular.module('phonecatControllers', ['templateservicemod', 'mylife', 'ongojour
               geodesic: true, //set to false if you want straight line instead of arc
               map: map,
             });
-            firstTime = false;
           }
+
           var drawLine = function (departure, arrival, percent, i, value) {
             percentFrac = percent / 100;
             var are_we_there_yet = google.maps.geometry.spherical.interpolate(departure, arrival, percentFrac);
@@ -1105,14 +1029,6 @@ angular.module('phonecatControllers', ['templateservicemod', 'mylife', 'ongojour
           drawLine(departure, arrival, percentComplete, i, value);
         };
 
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          draggable: true,
-          animation: google.maps.Animation.DROP,
-          center: center,
-          // styles: mapStyle
-        });
-
         function setMarker(status, n) {
           var position = new google.maps.LatLng(n.lat, n.lng);
           bounds.extend(position);
@@ -1130,8 +1046,9 @@ angular.module('phonecatControllers', ['templateservicemod', 'mylife', 'ongojour
         _.each(centers, function (n) {
           setMarker(false, n);
         });
+
         //static polylines starts here
-        var travelPath = new google.maps.Polyline({
+        travelPath = new google.maps.Polyline({
           path: centers,
           geodesic: true,
           strokeColor: 'grey',
@@ -1139,7 +1056,6 @@ angular.module('phonecatControllers', ['templateservicemod', 'mylife', 'ongojour
           strokeWeight: 1
         });
         travelPath.setMap(map);
-        // scrollPath.setMap(map);
 
         function smoothZoom(map, max, cnt) {
           if (cnt > max) {
@@ -1168,13 +1084,28 @@ angular.module('phonecatControllers', ['templateservicemod', 'mylife', 'ongojour
           var arrival = new google.maps.LatLng(centers[i].lat, centers[i].lng); //Set to whatever lat/lng you need for your arrival locationlat:
           step = 0;
           redLineDraw(i, departure, arrival, percentComplete, value);
+          var linesCount = line.length - 1;
+          //clearPolyLines starts
+          while ((linesCount >= (i + 1))&&(value)) {
+            console.log(i);
+            console.log(linesCount+"--->"+line[linesCount]);
+            if (!_.isEmpty(line[linesCount])) {
+              console.log("inside clearing lines");
+               line[linesCount].setMap(null);
+              line[linesCount]={};
+              console.log("line  " + linesCount + "  cleared");
+              console.log(line);
+            };
+            linesCount--;
+          };
+          //clearPolyLines ends
+          //draw succeeding polyLines
           if (i > 1) {
             pointsForLine(i - 1, 100);
-            count=centers.length;
-           while(count!=i){
-             console.log("inside WHile loop");
-           }
+            count = centers.length;
           };
+
+
         };
       }
     };
@@ -7686,6 +7617,7 @@ angular.module('phonecatControllers', ['templateservicemod', 'mylife', 'ongojour
 
 .controller('headerctrl', function ($scope, TemplateService, NavigationService, $state, $interval) {
   $scope.template = TemplateService;
+
   NavigationService.getProfile(globalGetProfile, function (err) {
     console.log(err);
   });
@@ -7733,12 +7665,12 @@ angular.module('phonecatControllers', ['templateservicemod', 'mylife', 'ongojour
 
 })
 
-.controller('AgentloginCtrl', function($scope, TemplateService, NavigationService, $timeout) {
-    $scope.template = TemplateService.changecontent("agent-login"); //Use same name of .html file
-    $scope.menutitle = NavigationService.makeactive("Agent Login"); //This is the Title of the Website
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
-
+.controller('AgentloginCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
+  $scope.template = TemplateService.changecontent("agent-login"); //Use same name of .html file
+  $scope.menutitle = NavigationService.makeactive("Agent Login"); //This is the Title of the Website
+  TemplateService.title = $scope.menutitle;
+  $scope.navigation = NavigationService.getnav();
+  $scope.oneAtATime = true;
 })
 
 .controller('languageCtrl', function ($scope, TemplateService, $translate, $rootScope) {
