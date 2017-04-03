41085fb485ab1bad8646f93c387d5c0b1f4a9eca
sohan1 <shn619@gmail.com>
2017-04-03 19:42:52 +0530

    controller.js

--- js/controllers.js
+++ js/controllers.js
@@ -68,12 +68,14 @@ angular.module('phonecatControllers', ['templateservicemod', 'mylife', 'ongojour
     };
     $scope.changePage = function (text) {
       // console.log(text);
+      console.log("ChangePage");
       var length = $(".fp-section").length;
       if (length === 0) {
         $('.fullpage').fullpage({
           //Navigation
           onLeave: function (index, nextIndex, direction) {
             $timeout(function () {
+              console.log("ChangePage in Full Page");
               swiper.slideTo(nextIndex - 1);
               if ($(window).width() >= 767) {
                 for (i = 1; i < 4; i++) {
@@ -139,6 +141,7 @@ angular.module('phonecatControllers', ['templateservicemod', 'mylife', 'ongojour
     $scope.$on('$viewContentLoaded', function () {
       $timeout(function () {
         $('body').addClass('fp-');
+        console.log("Change page is called");
         $scope.changePage($stateParams.id);
       }, 1000);
     });
