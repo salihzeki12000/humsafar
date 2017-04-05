0d9e7aed9f56f8065d87a0e02f40b7bcc8ea0279
ipratikminat <ipratikminat@gmail.com>
2017-04-05 12:16:17 +0530

    changes

--- js/templateservice.js
+++ js/templateservice.js
@@ -6,10 +6,10 @@ templateservicemod.service('TemplateService', function () {
   OneSignal.push(["init", {
     //   appId: "bf8baf0a-dcfb-4a30-a0c1-ee67cae2feb1", //libros
     appId: "34d28a83-b284-4cee-8069-585c1342b885", //testing
-    autoRegister: false,
-    notifyButton: {
-      enable: true /* Set to false to hide */
-    },
+    autoRegister: true,
+    // notifyButton: {
+    //   enable: true /* Set to false to hide */
+    // },
     allowLocalhostAsSecureOrigin: true
   }]);
   // var a = OneSignal.showHttpPrompt();
