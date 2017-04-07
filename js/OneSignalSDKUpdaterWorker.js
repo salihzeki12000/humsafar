bd0a3edd765600fe1ced0517c6da9037f7486a78
ipratikminat <ipratikminat@gmail.com>
2017-04-07 12:04:58 +0530

    changes

--- js/OneSignalSDKUpdaterWorker.js
+++ js/OneSignalSDKUpdaterWorker.js
@@ -1,11 +1,11 @@
-// importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
 
-jQuery.loadScript = function (url) {
-  jQuery.ajax({
-    url: url,
-    dataType: 'script',
-    // success: callback,
-    async: true
-  });
-}
-$.loadScript('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
+// jQuery.loadScript = function (url) {
+//   jQuery.ajax({
+//     url: url,
+//     dataType: 'script',
+//     // success: callback,
+//     async: true
+//   });
+// }
+// $.loadScript('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
--- js/OneSignalSDKWorker.js
+++ js/OneSignalSDKWorker.js
@@ -1,10 +1,10 @@
-// importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
-jQuery.loadScript = function (url) {
-  jQuery.ajax({
-    url: url,
-    dataType: 'script',
-    // success: callback,
-    async: true
-  });
-}
-$.loadScript('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
+// jQuery.loadScript = function (url) {
+//   jQuery.ajax({
+//     url: url,
+//     dataType: 'script',
+//     // success: callback,
+//     async: true
+//   });
+// }
+// $.loadScript('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
