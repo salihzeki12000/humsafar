a7a9c31761a624c98a146afa7d1f5fb30521cf1d
mitverma <amitwohlig@gmail.com>
2017-08-04 13:41:11 +0530

    gulp file add of fade image loading

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,7 @@ var jsArray = [
     './bower_components/angular-translate/angular-translate.js',
     './bower_components/lodash/lodash.js',
     './bower_components/angulartics/dist/angulartics.min.js',
+    './bower_components/angular-fade-image-loading/angular-fade-image-loading.min.js',
     // './bower_components/angulartics-google-analytics/dist/angulartics-google-analytics.min.js',
     // './bower_components/angular-mappy/build/angular-mappy.js',
     './js/mapping.js',
