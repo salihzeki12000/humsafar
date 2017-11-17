ff7a83c0f148164c4589a5aefbaded7280260560
mitverma <amitwohlig@gmail.com>
2017-11-17 12:29:18 +0530

    working on scroll

--- js/controllers.js
+++ js/controllers.js
@@ -329,6 +329,215 @@ angular.module('phonecatControllers', ['templateservicemod', 'mylife', 'ongojour
     // $scope.customLink = function() {
     //   window.open("https://play.google.com/store/apps/details?id=com.ascra.app.travellibro");
     // };
+
+//      var firstMobSlide;
+//     var secondMobSlide;
+//     var thirdMobSlide;
+//     var screenFirst;
+//     var screenSecond;
+//     var screenThird;
+//     // animation functions 
+//     (function($){
+//       var windowheight =  window.innerHeight,
+//       firstMobSlide = $('.mobile-screen-one'),
+//       secondMobSlide = $('.mobile-screen-sec'),
+//       thirdMobSlide = $('.mobile-screen-third'),
+//       screenFirst = $('.screen-first'),
+//       screenSecond = $('.screen-second'),
+//       screenThird = $('.screen-third');
+//       console.log(firstMobSlide,'firstMobSlide');
+//     })(jQuery);
+//     // init
+//     var ctrl = new ScrollMagic.Controller({
+//       globalSceneOptions: {
+//         triggerHook: 'onLeave'
+//       }
+//     });
+ 
+//     // Create scene
+//     $(".section-view").each(function() { 
+//       console.log('amit');
+//     new ScrollMagic.Scene({
+//       triggerElement: this,
+//       duration: '50%'
+//     })
+//     .setPin(this)
+//     .addTo(ctrl);
+//     });
+//     var iphoneContentHat = new TimelineMax();
+//      iphoneContentHat       
+//       .to(firstMobSlide, 1, {yPercent: -50, ease: Power4.easeOut})
+//       .fromTo(secondMobSlide, 1, {yPercent: 20, autoAlpha: 0, scale: 0.8}, {yPercent: 0, autoAlpha: 1, scale: 1, ease: Power4.easeOut}, '0')
+//       .fromTo(thirdMobSlide, 1, {autoAlpha: 0}, '-=0.3');
+ 
+//       new ScrollMagic.Scene({
+//       offset: windowheight*0.6,
+//       triggerElement: $('body')[0],
+//       duration: '80%'
+//     })
+//     .setTween(iphoneContentHat)
+//     .addTo(ctrl);
+
+//     var iphoneContent1Tl = new TimelineMax();
+//     iphoneContent1Tl       
+//     .to(screenFirst, 0.3, {yPercent: -30, autoAlpha: 0, ease: Power4.easeInOut})
+//     .fromTo(screenSecond, 1, {yPercent: 20, autoAlpha: 0}, {yPercent: 0, autoAlpha: 1, ease: Power4.easeInOut})
+//     .from(screenThird, 1, {autoAlpha: 0}, '-=0.7');
+ 
+//     new ScrollMagic.Scene({
+//       triggerElement: $('.innerS2 h2')[0],
+//       duration: '50%'
+//     })
+//     .setTween(iphoneContent1Tl)
+//     .addTo(ctrl);
+ 
+//   var iphoneContent2Tl = new TimelineMax();
+//   iphoneContent2Tl       
+//     .to($screenB, 0.3, {yPercent: -30, autoAlpha: 0, ease: Power4.easeInOut})
+//     .fromTo($screenC, 1, {yPercent: 20, autoAlpha: 0}, {yPercent: 0, autoAlpha: 1, ease: Power4.easeInOut})
+//     .from($innerS4, 1, {autoAlpha: 0}, '-=0.7');
+ 
+//   new ScrollMagic.Scene({
+//     triggerElement: $('.innerS3 h2')[0],
+//     duration: '50%'
+//   })
+// .setTween(iphoneContent2Tl)
+// .addTo(ctrl);
+var wh = window.innerHeight,
+  $iphone = $('.iphone'),
+  $innerS1 = $('.innerS1'),
+  $innerS2 = $('.innerS2'),
+  $innerS3 = $('.innerS3'),
+  $innerS4 = $('.innerS4'),
+  $screenHat = $('.screenHat'),
+  $screenA = $('.screenA'),
+  $screenB = $('.screenB'),
+  $screenC = $('.screenC');
+
+  // init
+  var ctrl = new ScrollMagic.Controller({
+      globalSceneOptions: {
+          triggerHook: 'onLeave'
+      }
+  });
+
+  // Create scene
+  $("section").each(function() {
+    
+    new ScrollMagic.Scene({
+      triggerElement: this,
+      duration: '50%'
+    })
+    .setPin(this)
+    .addTo(ctrl);
+   
+  });
+
+  // iPhone intro animation Tween
+  /*
+  var iphoneIntro = TweenMax.from($iphone, 1, {
+    yPercent: 50,
+    xPercent: 100,
+    ease: Cubic.easeOut
+  });
+  */
+
+  // iPhone intro animation Timeline
+  var iphoneIntroTl = new TimelineMax();
+  iphoneIntroTl
+    .from($iphone, 1, {yPercent: 50,xPercent: 100, ease: Power4.easeInOut})
+    .to($innerS1, 0.5, {opacity: 0, yPercent: -5, scale: 0.98}, '0');
+
+  // iPhone back to stylesheet position
+  new ScrollMagic.Scene({
+    duration: '70%'
+  })
+  .setTween(iphoneIntroTl)
+  .triggerElement($('body')[0])
+  .addTo(ctrl);
+  // iPhone intro animation Tween
+var iphoneIntro = TweenMax.from($iphone, 1, {
+    yPercent: 50,
+    xPercent: 100,
+    ease: Cubic.easeOut
+});
+// iPhone back to stylesheet position
+new ScrollMagic.Scene({
+    duration: '70%'
+})
+.setTween(iphoneIntro)
+.triggerElement($('body')[0])
+.addTo(ctrl);
+// iPhone back to stylesheet position
+new ScrollMagic.Scene({
+    duration: '70%'
+})
+.setTween(iphoneIntroTl)
+.triggerElement($('body')[0])
+.addTo(ctrl);
+var iphoneIntroTl = new TimelineMax();
+iphoneIntroTl
+    .from($iphone, 1, {yPercent: 50,xPercent: 100, ease: Power4.easeInOut})
+    .to($innerS1, 0.5, {opacity: 0, yPercent: -5, scale: 0.98}, '0');
+  
+  // Animate the hat up, letter A in and fade in content of section 2
+  var iphoneContentHat = new TimelineMax();
+  iphoneContentHat    
+    .to($screenHat, 1, {yPercent: -50, ease: Power4.easeOut})
+    .fromTo($screenA, 1, {yPercent: 20, autoAlpha: 0, scale: 0.8}, {yPercent: 0, autoAlpha: 1, scale: 1, ease: Power4.easeOut}, '0')
+    .from($innerS2, 1, {autoAlpha: 0}, '-=0.3');
+
+  new ScrollMagic.Scene({
+    offset: wh*0.6,
+    triggerElement: $('body')[0],
+    duration: '80%'
+  })
+  .setTween(iphoneContentHat)
+  .addIndicators()
+  .addTo(ctrl);
+
+  // Animate letter A out, letter B in and fade in content of section 3
+  var iphoneContent1Tl = new TimelineMax();
+  iphoneContent1Tl    
+    .to($screenA, 0.3, {yPercent: -30, autoAlpha: 0, ease: Power4.easeInOut})
+    .fromTo($screenB, 1, {yPercent: 20, autoAlpha: 0}, {yPercent: 0, autoAlpha: 1, ease: Power4.easeInOut})
+    .from($innerS3, 1, {autoAlpha: 0}, '-=0.7');
+
+  new ScrollMagic.Scene({
+    triggerElement: $('.innerS2 h2')[0],
+    duration: '50%'
+  })
+  .setTween(iphoneContent1Tl)
+  .addTo(ctrl);
+
+  // Animate letter B out, letter C in and fade in content of section 4
+  var iphoneContent2Tl = new TimelineMax();
+  iphoneContent2Tl    
+    .to($screenB, 0.3, {yPercent: -30, autoAlpha: 0, ease: Power4.easeInOut})
+    .fromTo($screenC, 1, {yPercent: 20, autoAlpha: 0}, {yPercent: 0, autoAlpha: 1, ease: Power4.easeInOut})
+    .from($innerS4, 1, {autoAlpha: 0}, '-=0.7');
+
+  new ScrollMagic.Scene({
+    triggerElement: $('.innerS3 h2')[0],
+    duration: '50%'
+  })
+  .setTween(iphoneContent2Tl)
+  .addTo(ctrl);
+    // animation functions end 
+
+    // var controllerScroll = new ScrollMagic.Controller();
+    // var mobileHolder = $('.mobileHolder');
+    // $(document).ready(function(){
+    //   var scene = new ScrollMagic.Scene({
+    //     triggerElement: "#mobileHolder",
+    //     duration: 70,
+    //     reverse: true
+    //   })
+    //   .setTween(mobileHolder,1,{yPercent: 50,xPercent: 100, ease: Power4.easeInOut},{yPercent: 50,xPercent: 100, ease: Power4.easeInOut})
+    //   .setPin('#mobile')
+    //   .addIndicators({name: "phone (duration: 00)"}) // add indicators (requires plugin)
+    //   .addTo(controllerScroll);
+    // });
   })
 
   .controller('LoginCtrl', function($scope, TemplateService, NavigationService, Agent, cfpLoadingBar, $timeout, $uibModal, $interval, $state, $http) {
